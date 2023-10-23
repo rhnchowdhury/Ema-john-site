b0125bdf17a76786e449b40c6b684bd56ced3cbf
Raihan <rhnchdri@gmail.com>
2023-10-24 05:43:32 +0600

    summary ll added

--- src/Components/Carts/Cart.js
+++ src/Components/Carts/Cart.js
@@ -2,11 +2,29 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
+
+    // total price & shipping calculate
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    };
+
+    // tax calculate for 10%
+    const tax = (total * 0.1).toFixed(2);
+
+    // Grand total calculate
+    const grandTotal = total + shipping + parseFloat(tax);
+
     return (
         <div className='cart-section'>
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price:</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Price: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };

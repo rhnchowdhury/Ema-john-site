a6815d988c1621e2ea034c9800cf04865080d12b
Raihan <rhnchdri@gmail.com>
2023-10-23 11:50:35 +0600

    header design

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
     );
 };
 
